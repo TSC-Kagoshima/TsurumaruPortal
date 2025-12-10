e74cc9d90c869b259da3f40fa774ad8e77a58550
TSC-Kagoshima <tsurumarucouncil@gmail.com>
2025-12-10 21:11:41 +0900

    wdrywestyh

--- seitokai/classmatch/manage/js/front.js
+++ seitokai/classmatch/manage/js/front.js
@@ -26,6 +26,5 @@ document.addEventListener('DOMContentLoaded', () => {
       document.querySelector('.logout').classList.add('.visible')
     }
   }
-  
-  notice();
+
 });
--- seitokai/classmatch/manage/js/scriptv1.1.0.js
+++ seitokai/classmatch/manage/js/scriptv1.1.0.js
@@ -21,22 +21,6 @@ async function login() {
         alert('支部、またはパスワードが違います。実施学期を間違えた場合は前のページに戻ってください。');
       }      
   }    
-function loginSuccess() {
-      localStorage.setItem("branch", document.getElementById('game-branch').value);
-      document.querySelector('.popup-select-game').classList.add('success');
-
-      document.getElementById('commu-popup-content').innerHTML = "ログインしました。" 
-        + new URLSearchParams(window.location.search).get('term') + "：" + document.getElementById('game-branch').value;
-        loadGame();
-        
-        document.querySelector('.logout').classList.add('visible');
-        document.querySelector('header-title').value = document.getElementById('game-branch').value;
-}
-function logout() {
-   localStorage.clear('branch');
-   window.location.href = "";
-}
-
 
 
 let sendbutton = false;
@@ -86,22 +70,6 @@ function sendGameResult() {
 
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  const params = new URLSearchParams(window.location.search);
-  const value = params.get('term'); 
-
-  const branchCache = localStorage.getItem('branch');
-  const termCache = localStorage.getItem('term')
-  if(branchCache) {
-    if(termCache == value) {    
-      const popup = document.querySelector('.popup-select-game');
-      popup.classList.add('success');
-      loadGame();
-      document.querySelector('.logout').classList.add('.visible')
-    }
-  }
-  
-});
 function loadGame() {
     const params = new URLSearchParams(window.location.search);
     const value = params.get('term'); 
