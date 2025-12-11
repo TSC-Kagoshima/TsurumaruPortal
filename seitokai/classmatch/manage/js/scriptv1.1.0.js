de32e0c964569767d72ce96b3753c6a3ebb1af67
TSC-Kagoshima <tsurumarucouncil@gmail.com>
2025-12-11 18:59:12 +0900

    gerg

--- seitokai/classmatch/manage/js/scriptv1.1.0.js
+++ seitokai/classmatch/manage/js/scriptv1.1.0.js
@@ -100,7 +100,7 @@ if (gameteam) {
 }
 }
 
-function notice() {
+async function notice() {
     const response = await fetch(url, {
       method: "GET",
       headers: { "Content-Type": "application/json" },
